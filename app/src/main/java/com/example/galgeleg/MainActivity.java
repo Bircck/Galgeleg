ac5866f5950cbcea427cbb6d06630e8713bc85ff
tobias <tobiasobk@gmail.com>
2020-10-23 18:46:49 +0200

    Opgaveformulering/test commit

--- app/src/main/java/com/example/galgeleg/MainActivity.java
+++ app/src/main/java/com/example/galgeleg/MainActivity.java
@@ -4,20 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-/*ting vi skal have med
-
-    starte projektet    ✓
-
-    Hovedmenu           x
-    Selve spillet       x
-        du har vundet skærmbillede     x
-        du har tab skærmbillede        x
-    Hjælp skærm         x
-    Highscore           x
-
-
- */
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
